8b08347a166b0d9bc6cd96e04883014b9685bb85
Samuel Hatcliff <samuelhatcliff@Samuels-MacBook-Air.local>
2022-01-24 19:08:44 -0600

    changes to users-model test file

--- test_user_model.py
+++ test_user_model.py
@@ -26,33 +26,73 @@ from app import app
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
-db.create_all()
-
-
 class UserModelTestCase(TestCase):
-    """Test views for messages."""
+#     """Test views for messages."""
 
     def setUp(self):
-        """Create test client, add sample data."""
+    #     """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        db.drop_all()
+        db.create_all()
+
+        u1 = User.signup("test1", "email1@email.com", "password", None)
+        u1.id = 1
+        u2 = User.signup("test2", "email2@email.com", "password", None)
+        u2.id = 2
+
+        
+
+        user1 = User.query.get(1)
+        user2 = User.query.get(2)
+
+        db.session.add(user1)
+        db.session.add(user2)
+        db.session.commit()
+                        
 
         self.client = app.test_client()
+    
+    # def tearDown(self):
+    #     res = super().tearDown()
+    #     db.session.rollback()
+    #     return res
 
-    def test_user_model(self):
-        """Does basic model work?"""
+    # def test_user_model(self):
+    # #     """Does basic model work?"""
+    #     user1 = User.query.get(1)
 
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
 
-        db.session.add(u)
-        db.session.commit()
+    #     # User should have no messages & no followers
+    #     self.assertEqual(len(user1.messages), 0)
+    #     self.assertEqual(len(user1.followers), 0)
 
-        # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+    # def test_repr_method(self):
+        # User.query.delete()
+        # u = User(
+        #     email="test@test.com",
+        #     username="testuser",
+        #     password="HASHED_PASSWORD"
+        # )
+
+        # db.session.add(u)
+        # db.session.commit()
+
+
+        # repr = f"<User #1: testuser, test@test.com>"
+
+        # self.assertEqual(repr, self.u1)
+
+    # def is_following(self):
+    #     User.query.delete()
+    #     u1 = User(
+    #         email="test@test.com",
+    #         username="testuser",
+    #         password="HASHED_PASSWORD"
+    #     )
+    #     u2 = User(
+    #         email="test@test2.com",
+    #         username="testuser2",
+    #         password="HASHED_PASSWORD2"
+    #     )
+    #     u1.following.append(u2)
+    #     self.assertIn(u1.following, u2)
